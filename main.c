1a40558c276fc88cb43d168744384059c431e067
Mozart Kandie <mozartkandie@gmail.com>
2026-04-23 00:02:36 +0300

    mozart added this

--- main.c
+++ main.c
@@ -71,8 +71,16 @@ int main(int argc, char **argv) {
 
     ParseTreeNode *tree = parser_parse(&ts);
 
+
+
     /* 4.5. Semantic Analysis / Symbol Table Pass Placeholder  */
     /* tree = semantic_analyze(tree); */
+    /* ADD THESE LINES TO SEE YOUR PARSER'S ENGINE DATA */
+    printf("\n=== GRAMMAR ANALYSIS ===\n");
+    print_first_sets();
+    print_follow_sets();
+    print_parse_table();
+    printf("========================\n\n");
 
     /* ── 5. Output ───────────────────────────────────────────── */
     if (tree && error_count == 0) {
